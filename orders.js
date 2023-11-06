778c6198758aa568a3248687372c3f61a73927f9
yasiBobo <yasi00bobo@gmail.com>
2023-11-06 14:06:42 +0330

    save

---
+++ orders.js
@@ -0,0 +1,44 @@
+const Orders = [
+    {
+        productName: "Product Name",
+        productNumber: "000",
+        paymentStatus: "PS",
+        shipping: "Status"
+    },
+    {
+        productName: "Product Name",
+        productNumber: "000",
+        paymentStatus: "PS",
+        shipping: "Status"
+    },
+    {
+        productName: "Product Name",
+        productNumber: "000",
+        paymentStatus: "PS",
+        shipping: "Status"
+    },
+    {
+        productName: "Product Name",
+        productNumber: "000",
+        paymentStatus: "PS",
+        shipping: "Status"
+    },
+    {
+        productName: "Product Name",
+        productNumber: "000",
+        paymentStatus: "PS",
+        shipping: "Status"
+    },
+    {
+        productName: "Product Name",
+        productNumber: "000",
+        paymentStatus: "PS",
+        shipping: "Status"
+    },
+    {
+        productName: "Product Name",
+        productNumber: "000",
+        paymentStatus: "PS",
+        shipping: "Status"
+    }
+]
